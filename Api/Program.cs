32aa39fa16fc3641d4e0d5dc3a4fe146489e3068
komp <komp@DESKTOP-2IE6JU7>
2025-08-07 21:25:51 +0300

    Basic structure

---
+++ Api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.Run();
